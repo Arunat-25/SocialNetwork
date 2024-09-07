c52d42d701b4994d748a340e1724f7fb01b7b36a
Arunat-25 <128986470+Arunat-25@users.noreply.github.com>
2024-09-07 22:39:00 +0700

    фигня

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'chat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
